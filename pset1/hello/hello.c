ef5a10bd5f6c6cbb5da035ca683e08e61b7c8368
Eduardo Batista <58524485+eduhbg@users.noreply.github.com>
2021-03-18 08:46:42 -0300

    delete pset1

--- pset1/hello/hello.c
+++
@@ -1,9 +0,0 @@
-#include <cs50.h>
-#include <stdio.h>
-
-int main(void)
-{
-    // Get the user name and say hello
-    string name = get_string("What's your name?\n");
-    printf("hello, %s\n", name);
-}
